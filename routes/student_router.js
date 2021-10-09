acb077e82cd5871f481fa3750312239f868b6b35
Yo4v <57178162+Yo4v@users.noreply.github.com>
2021-10-09 15:49:50 +0300

    Add files via upload

--- routes/student_router.js
+++ routes/student_router.js
@@ -1,13 +1,6 @@
 // ייבוא מודלים נדרשים
 const express = require('express');
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 const alert = require('alert');
-
-/* ^
-האם יש צורך לכתוב את זה גם פה וגם בקובץ הראשי? או שמספיק רק בקובץ אחד? 
-אם כן אז פה או או בראשי? */
 
 // הגדרת ראוטר הסטודנט וכן ייבוא מודל הסטודנט
 const studentRoute = express.Router();
@@ -15,34 +8,37 @@ const StudentModel = require('../models/student_model');
 
 // יצירת סטודנט חדש במערכת
 studentRoute.get('/add', (req, res) => {
-  console.log('Open add page');
-  console.log(req.body);
-  let id = null;
-  let sug_toar = { ba: false, ma: false, phd: false };
-  res.render('add.pug', { id: id, sug_toar: sug_toar });
+  const [id, sug_toar] = [null, { ba: false, ma: false, phd: false }];
+  res.render('add.pug', { id, sug_toar });
 });
 studentRoute.post('/add', (req, res) => {
-  let s_id = req.body.id.trim();
-  let s_name = req.body.name.trim();
-  let s_city = req.body.city.trim();
-  let toar = req.body.toar;
-  let sug_toar = { ba: false, ma: false, phd: false };
+  const [s_id, s_name, s_city, toar] = [
+    req.body.id.trim(),
+    req.body.name.trim(),
+    req.body.city.trim(),
+    req.body.toar,
+  ];
+  const [sug_toar, addstudent] = [
+    { ba: false, ma: false, phd: false },
+    new StudentModel(req.body),
+  ];
   sug_toar[toar] = true;
-  let addstudent = new StudentModel(req.body);
-  addstudent.save(function (err, newstudent) {
+  addstudent.save((err, newstudent) => {
     let error = null;
     if (err) {
       error = err.message;
+      res.json('FAIL');
+      alert('הוספת הסטודנט למערכת נתקלה בשגיאה');
       console.log('Could not save student', err.message);
     }
 
-// פעולה רצוייה בהתאם למצב הריצה של היישום
+    // פעולה רצוייה בהתאם למצב הריצה של היישום
     if (global.runmode === 'JSON') {
       res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify(newstudent));
+      res.json(JSON.stringify(newstudent));
       alert('JSON פרטי הסטודנט החדש בפורמט');
     } else {
-      alert('הוספת הסטודנט/ית למערכת בוצעה בהצלחה');
+      alert('הוספת הסטודנט למערכת בוצעה בהצלחה');
       console.log(`Student added successfully: ${newstudent}`);
       res.render('add.pug', {
         id: s_id,
@@ -57,8 +53,10 @@ studentRoute.post('/add', (req, res) => {
 
 // קריאת רשימת הסטודנטים הנמצאים במערכת
 studentRoute.get('/', (req, res) => {
-  let sug_toar = { ba: false, ma: false, phd: false };
-  let avg = req.params.avg;
+  const [sug_toar, avg] = [
+    { ba: false, ma: false, phd: false },
+    req.params.avg,
+  ];
   StudentModel.find({}, (err, students) => {
 
 // פעולה רצוייה בהתאם למצב הריצה של היישום
@@ -66,7 +64,7 @@ studentRoute.get('/', (req, res) => {
       if (global.runmode === 'JSON') {
         res.setHeader('Content-Type', 'application/json');
         res.json(JSON.stringify(students));
-        alert('JSON רשימת הסטדודנטים הקיימים במסד בפורמט');
+        alert('JSON רשימת הסטודנטים הקיימים במסד בפורמט');
       } else {
         res.render('index.pug', {
           students: students,
@@ -74,19 +72,16 @@ studentRoute.get('/', (req, res) => {
           avg: avg,
         });
       }
-    } else {
-      alert('Could not retrieve students');
     }
   });
 });
 
-// סינון רשימת הסטודנדטים
+// סינון רשימת הסטודנטים
 studentRoute.post('/', (req, res) => {
-  console.log('Filter: ', req.body);
-  let sug_toar = { ba: false, ma: false, phd: false };
-  let { city, toar, avg } = req.body;
+  const sug_toar = { ba: false, ma: false, phd: false };
+  const { city, toar, avg } = req.body;
 
-// יצירת אובייקט סינון עם שימוש במבנה סינון אגרגטיבי  
+// יצירת אובייקט סינון עם שימוש במבנה סינון אגרגטיבי
   let filter = { $expr: { $and: [] } };
 
 // הכנסת שדה ה"עיר" למשתנה במידה והוא לא ריק
@@ -115,7 +110,12 @@ studentRoute.post('/', (req, res) => {
         res.json(JSON.stringify(filter));
         alert('JSON הסטודנטים שמתאימים לסינון בפורמט');
       } else {
-        console.log(`The students who meet the criteria set are: ${students}`);
+        console.log(
+          'The students who meet the',
+          req.body,
+          'criteria are:',
+          students
+        );
         res.render('index.pug', {
           students: students,
           sug_toar: sug_toar,
@@ -123,9 +123,6 @@ studentRoute.post('/', (req, res) => {
           avg: avg,
         });
       }
-    } else {
-      res.send('No students found matching criteria set, please try again');
-      alert('לא נמצאו סטודנטים התואמים את הגדרות הסינון');
     }
   });
 });
@@ -133,68 +130,113 @@ studentRoute.post('/', (req, res) => {
 // עדכון פרטי סטודנט
 studentRoute.get('/update/:id', (req, res) => {
   StudentModel.findOne({ id: req.params.id }, (err, students) => {
-    let studentID = students.id;
-    let studentName = students.name;
-    let studentCity = students.city;
-    let studentToar = students.toar;
-    let studentCourses = students.courses;
+    const [s_id, s_name, s_city, s_toar, s_courses] = [
+      students.id,
+      students.name,
+      students.city,
+      students.toar,
+      students.courses,
+    ];
     if (!err) {
       res.render('update.pug', {
-        id: studentID,
-        name: studentName,
-        city: studentCity,
-        toar: studentToar,
-        courses: studentCourses,
+        id: s_id,
+        name: s_name,
+        city: s_city,
+        toar: s_toar,
+        courses: s_courses,
       });
     } else {
-      alert('Could not retrieve students');
+      alert('לא נמצאו סטודנטים במערכת');
     }
   });
 });
 
-// עדכון סטודנט - כרגע ריק
-
-// הוספת קורס
+// פעולה בהתאם לסוג הטופס
 studentRoute.post('/update/:id', async (req, res) => {
-  try {
-    console.log('Adding new course...');
-    let cid = req.body.cid.trim();
-    let grade = req.body.grade.trim();
-    let addcourse = { cid, grade };
-    let student = await StudentModel.findOne({ id: req.params.id });
-    student.courses.push(addcourse);
-    await student.save();
-    alert('הוספת הקורס למערכת בוצעה בהצלחה');
-    console.log(`Course added successfully: ID - ${cid} ; Grade - ${grade}`);
 
+// עדכון סטודנט
+  if ('updatestudent' === req.body.action) {
+    try {
+      const student = await StudentModel.findOneAndUpdate(
+        { id: req.params.id },
+        {
+          $set: {
+            name: req.body.name.trim(),
+            city: req.body.city.trim(),
+          },
+        },
+        { new: true }
+      );
+      alert('עדכון הסטודנט במערכת בוצע בהצלחה');
 
 // פעולה רצוייה בהתאם למצב הריצה של היישום
-    if (global.runmode === 'JSON') {
-      res.json(student);
-      alert('JSON הקורס נוסף למערכת בפורמט');
-    } else {
-      res.render('update.pug', {
-        id: student.id,
-        name: student.name,
-        city: student.city,
-        toar: student.toar,
-        courses: student.courses,
-      });
+      if (global.runmode === 'JSON') {
+        res.json(student);
+        alert('JSON עדכון הסטודנט בפורמט');
+      } else {
+        res.render('update.pug', {
+          id: student.id,
+          name: student.name,
+          city: student.city,
+          toar: student.toar,
+          courses: student.courses,
+        });
+      }
+    } catch (err) {
+      if (global.runmode === 'JSON') {
+        res.json('FAIL');
+        alert('JSON לא ניתן לעדכן סטודנט בפורמט');
+      } else {
+        error = err.message;
+        res.render('update.pug', {
+          id: student.id,
+          name: student.name,
+          city: student.city,
+          toar: student.toar,
+          courses: student.courses,
+        });
+      }
     }
-  } catch (err) {
-    console.log('Adding course error!', err.message);
-    if (global.runmode === 'JSON') {
-      res.json('fail');
-      alert('JSON לא ניתן להוסיף קורס בפורמט');
-    } else {
-      error = err.message;
-      res.render('update.pug', {
-        id: student.id,
-        name: student.name,
-        city: student.city,
-        toar: student.toar,
-        courses: student.courses,
-      });
+
+// הוספת קורס
+  } else if ('addcourse' === req.body.action) {
+    try {
+      const [cid, grade] = [req.body.cid.trim(), req.body.grade.trim()];
+      const [addcourse, student] = [
+        { cid, grade },
+        await StudentModel.findOne({ id: req.params.id }),
+      ];
+      student.courses.push(addcourse);
+      await student.save();
+      alert('הוספת הקורס למערכת בוצעה בהצלחה');
+
+// פעולה רצוייה בהתאם למצב הריצה של היישום
+      if (global.runmode === 'JSON') {
+        res.json(student);
+        alert('JSON הקורס נוסף למערכת בפורמט');
+      } else {
+        res.render('update.pug', {
+          id: student.id,
+          name: student.name,
+          city: student.city,
+          toar: student.toar,
+          courses: student.courses,
+        });
+      }
+    } catch (err) {
+      if (global.runmode === 'JSON') {
+        res.json('FAIL');
+        alert('JSON לא ניתן להוסיף קורס בפורמט');
+      } else {
+        error = err.message;
+        res.render('update.pug', {
+          id: student.id,
+          name: student.name,
+          city: student.city,
+          toar: student.toar,
+          courses: student.courses,
+        });
+      }
     }
   }
 });
@@ -202,23 +244,22 @@ studentRoute.post('/update/:id', async (req, res) => {
 // מחיקת סטודנט
 studentRoute.post('/delete/:id', (req, res) => {
   let ids = req.params.id;
-  console.log('Asked to delete a student: ', req.params.id);
   StudentModel.deleteOne({ id: ids }, (err) => {
 
 // פעולה רצוייה בהתאם למצב הריצה של היישום
     if (!err) {
       if (global.runmode === 'JSON') {
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify('1'));
+        res.json(JSON.stringify('1'));
         alert('JSON הסטודנט נמחק מהמערכת בפורמט');
       } else {
-        console.log(`Student ID ${req.params.id} deleted successfully`);
         alert('הסטודנט נמחק מהמערכת בהצלחה');
+        console.log(`Student ID ${req.params.id} deleted successfully`);
         res.redirect('/student');
       }
     } else {
       if (global.runmode === 'JSON') {
-        res.send(JSON.stringify('0'));
+        res.json(JSON.stringify('0'));
         alert('JSON שגיאה במחיקת הסטודנט בפורמט');
       } else {
         res.redirect('/student');
